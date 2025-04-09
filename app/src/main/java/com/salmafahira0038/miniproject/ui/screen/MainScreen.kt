2efe2b27692f6b98c4cf8133f22369d6dbc4c011
Salma Fahira Azzahra <salmafahiraa05@gmail.com>
2025-04-09 12:36:09 +0700

    Menambahkan Radio button dan Tombol hitung

--- app/src/main/java/com/salmafahira0038/miniproject/ui/screen/MainScreen.kt
+++ app/src/main/java/com/salmafahira0038/miniproject/ui/screen/MainScreen.kt
@@ -5,19 +5,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -31,7 +40,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -82,10 +93,17 @@ fun ScreenContent(modifier: Modifier = Modifier) {
 
     val expandedAwal = remember { mutableStateOf(false) }
 
-    val selectedAwal = remember { mutableIntStateOf(pilihKonversi[0])  }
+    val selectedAwal = remember { mutableIntStateOf(pilihKonversi[0]) }
 
-    Column (
-        modifier = modifier.fillMaxSize().padding(16.dp),
+    val radioOption = listOf(
+        stringResource(id = R.string.pilih_celcius),
+        stringResource(id = R.string.pilih_fahrenheit),
+        stringResource(id = R.string.pilih_kelvin),
+    )
+    var pilihSuhuAwal by remember { mutableStateOf(radioOption[0]) }
+
+    Column(
+        modifier = modifier.fillMaxSize().verticalScroll(rememberScrollState()).padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
@@ -96,8 +114,8 @@ fun ScreenContent(modifier: Modifier = Modifier) {
         )
         OutlinedTextField(
             value = suhu,
-            onValueChange = {suhu = it},
-            label = { Text(text = stringResource(R.string.masukkan_suhu))},
+            onValueChange = { suhu = it },
+            label = { Text(text = stringResource(R.string.masukkan_suhu)) },
 //            trailingIcon = { Text(text = "kg") },
             singleLine = true,
             keyboardOptions = KeyboardOptions(
@@ -106,52 +124,90 @@ fun ScreenContent(modifier: Modifier = Modifier) {
             ),
             modifier = Modifier.fillMaxWidth()
         )
-            ExposedDropdownMenuBox(
-                expanded = expandedAwal.value,
-                onExpandedChange = {
-                    expandedAwal.value = !expandedAwal.value
-                }
+
+            Row (
+                modifier = Modifier.padding(top = 6.dp).border(1.dp, Color.LightGray, RoundedCornerShape(4.dp))
             ) {
-                TextField(
-                    value = stringResource(selectedAwal.intValue),
-                    onValueChange = {},
-                    readOnly = true,
-                    label = { Text(text = stringResource(R.string.pilih_konversi)) },
-                    trailingIcon = {
-                        ExposedDropdownMenuDefaults.TrailingIcon(expanded = expandedAwal.value)
-                    },
-                    textStyle = TextStyle(fontSize = 15.sp),
-                    singleLine = true,
-                    modifier = Modifier
-                        .menuAnchor()
-                        .width(500.dp)
-                )
-                ExposedDropdownMenu(
-                    expanded = expandedAwal.value,
-                    onDismissRequest = { expandedAwal.value = false }
-                ) {
-                    pilihKonversi.forEach { resId ->
-                        DropdownMenuItem(
-                            text = { Text(text = stringResource(resId)) },
-                            onClick = {
-                                selectedAwal.intValue = resId
-                                expandedAwal.value = false
-                            }
-                        )
-                    }
+                radioOption.forEach { text ->
+                    ConvertionOption(
+                        label = text,
+                        isSelected = pilihSuhuAwal == text,
+                        modifier = Modifier
+                            .selectable(
+                                selected = pilihSuhuAwal == text,
+                                onClick = { pilihSuhuAwal = text },
+                                role = Role.RadioButton
+                            )
+                            .weight(2f).padding(9.dp)
+                    )
+                }
+            }
+
+        ExposedDropdownMenuBox(
+            expanded = expandedAwal.value,
+            onExpandedChange = {
+                expandedAwal.value = !expandedAwal.value
+            }
+        ) {
+            TextField(
+                value = stringResource(selectedAwal.intValue),
+                onValueChange = {},
+                readOnly = true,
+                label = { Text(text = stringResource(R.string.pilih_konversi)) },
+                trailingIcon = {
+                    ExposedDropdownMenuDefaults.TrailingIcon(expanded = expandedAwal.value)
+                },
+                textStyle = TextStyle(fontSize = 15.sp),
+                singleLine = true,
+                modifier = Modifier
+                    .menuAnchor()
+                    .width(500.dp)
+            )
+            ExposedDropdownMenu(
+                expanded = expandedAwal.value,
+                onDismissRequest = { expandedAwal.value = false }
+            ) {
+                pilihKonversi.forEach { resId ->
+                    DropdownMenuItem(
+                        text = { Text(text = stringResource(resId)) },
+                        onClick = {
+                            selectedAwal.intValue = resId
+                            expandedAwal.value = false
+                        }
+                    )
                 }
             }
         }
-}
-
-
-
-
-@Preview(showBackground = true)
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
-@Composable
-fun MainScreenPreview() {
-    MiniProjectTheme {
-        MainScreen()
+        Button(
+            onClick = {},
+            modifier = Modifier.padding(top = 8.dp),
+            contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
+        ) {
+            Text(text = stringResource(R.string.hitung))
+        }
     }
 }
+    @Composable
+    fun ConvertionOption(label: String, isSelected: Boolean, modifier: Modifier) {
+        Row(
+            modifier = modifier,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            RadioButton(selected = isSelected, onClick = null)
+            Text(
+                text = label,
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.padding(start = 8.dp)
+            )
+        }
+    }
+
+
+    @Preview(showBackground = true)
+    @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+    @Composable
+    fun MainScreenPreview() {
+        MiniProjectTheme {
+            MainScreen()
+        }
+    }
